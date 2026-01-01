a590b576721072619193ace86a1ce95e9ab4ad4f
Ans Saeed <anssaeed.pk@gmail.com>
2026-01-01 22:44:43 +0500

    initial commit

---
+++ src/components/Button.tsx
@@ -0,0 +1,13 @@
+import { HTMLAttributes } from "react";
+import {cva} from "class-variance-authority"
+const classes=cva('',{
+    variants: {
+        variant:{
+            primary: '',
+            secondary: '',
+        }
+    }
+})
+export default function Button(props:{ variant: "primary" | "secondary"} & HTMLAttributes<HTMLButtonElement>){
+    return <button></button>
+}
